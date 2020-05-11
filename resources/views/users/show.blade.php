1ec1e50c556c1cace971f98bf177b55b648f5f67
xunshan <244907263@qq.com>
2020-05-11 21:31:25 +0800

    显示个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,28 +5,30 @@
         <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
             <div class="card ">
                 <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
- <div class="card-body">
-                <h5><strong>个人简介</strong></h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                <hr>
-                <h5><strong>注册于</strong></h5>
-                <p>January 01 1901</p>
+                <div class="card-body">
+                    <h5><strong>个人简介</strong></h5>
+                    <p>{{ $user->introduction }}</p>
+                    <hr>
+                    <h5><strong>注册于</strong></h5>
+                    <p>{{ $user->created_at->diffForHumans() }}</p>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-        <div class="card ">
-            <div class="card-body">
-                <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
+        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+            <div class="card ">
+                <div class="card-body">
+                    <h1 class="mb-0" style="font-size:22px;">{{ $user->name }}
+                        <small>{{ $user->email }}</small>
+                    </h1>
+                </div>
+            </div>
+            <hr>
+            {{-- 用户发布的内容 --}}
+            <div class="card ">
+                <div class="card-body">
+                    暂无数据 ~_~
+                </div>
             </div>
         </div>
-        <hr>
-        {{-- 用户发布的内容 --}}
-        <div class="card ">
-            <div class="card-body">
-                暂无数据 ~_~
-            </div>
-        </div>
-    </div>
     </div>
 @stop
