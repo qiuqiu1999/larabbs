c26af289e764405630ef053cef3a5616cc7ad653
xunshan <244907263@qq.com>
2020-05-12 13:56:00 +0800

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -38,5 +38,8 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-
+// 文章
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+// 分类
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
