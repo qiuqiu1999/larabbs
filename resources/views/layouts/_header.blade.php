29d9f8c1bc446dc2de69e10e2c15bc961e706ab3
xunshan <244907263@qq.com>
2020-05-12 14:41:37 +0800

    用户话题列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -11,11 +11,21 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item {{ active_class(if_route('topics.index')) }}"><a class="nav-link" href="{{ route('topics.index') }}">话题</a></li>
-                <li class="nav-item {{ category_nav_active(1) }}"><a class="nav-link" href="{{ route('categories.show', 1) }}">分享</a></li>
-                <li class="nav-item {{ category_nav_active(2) }}"><a class="nav-link" href="{{ route('categories.show', 2) }}">教程</a></li>
-                <li class="nav-item {{ category_nav_active(3) }}"><a class="nav-link" href="{{ route('categories.show', 3) }}">问答</a></li>
-                <li class="nav-item {{ category_nav_active(4) }}"><a class="nav-link" href="{{ route('categories.show', 4) }}">公告</a></li>
+                <li class="nav-item {{ active_class(if_route('topics.index')) }}"><a class="nav-link"
+                                                                                     href="{{ route('topics.index') }}">话题</a>
+                </li>
+                <li class="nav-item {{ category_nav_active(1) }}"><a class="nav-link"
+                                                                     href="{{ route('categories.show', 1) }}">分享</a>
+                </li>
+                <li class="nav-item {{ category_nav_active(2) }}"><a class="nav-link"
+                                                                     href="{{ route('categories.show', 2) }}">教程</a>
+                </li>
+                <li class="nav-item {{ category_nav_active(3) }}"><a class="nav-link"
+                                                                     href="{{ route('categories.show', 3) }}">问答</a>
+                </li>
+                <li class="nav-item {{ category_nav_active(4) }}"><a class="nav-link"
+                                                                     href="{{ route('categories.show', 4) }}">公告</a>
+                </li>
             </ul>
 
             <!-- Left Side Of Navbar -->
@@ -40,9 +50,9 @@
                             <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" id="logout" href="#">
-                                <form action="{{ route('logout') }}" method="POST">
+                                <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗')">
                                     {{ csrf_field() }}
-                                    <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                                    <button class=" btn btn-block btn-danger" type="submit" name="button">退出</button>
                                 </form>
                             </a>
                         </div>
