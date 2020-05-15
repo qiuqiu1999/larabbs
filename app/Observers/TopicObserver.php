ae89d49e5d4bd92319a4046bac0bc1e23fd3c750
xunshan <244907263@qq.com>
2020-05-15 15:53:57 +0800

    修复XSS攻击漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -18,4 +18,10 @@ class TopicObserver
     {
         //
     }
+
+    public function saving(Topic $topic)
+    {
+        $topic->body = clean($topic->body, 'user_topic_body');
+        $topic->excerpt = make_excerpt($topic->body);
+    }
 }
